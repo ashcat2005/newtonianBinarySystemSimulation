4c5bea33a05ae02ff0d23f0f7c48c84295d053db
Nicolás A. Niño <58050431+Ste1nb0cK@users.noreply.github.com>
2021-06-18 16:16:28 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -59,24 +59,6 @@ def vr(r,phi,vphi,p,e):
     '''
     return 1/p * r**2 * e * np.sin(phi) * vphi
 
-#Define the function that updates the state of the system
-def updateState(q,dt):
-    '''Takes as input an array and a time step. Returns the updated state of the system
-    as an array'''
-    qUpdated = np.zeros(4)
-    #Calculate the new phi
-    phiNew = dphi(q[3],dt) + q[1]
-    qUpdated[1] = phiNew
-    #Calculate the new r
-    rNew = r(q[1],p,e)
-    qUpdated[0] = rNew
-    #New phi dot
-    vphiNew = vphi(rNew,L)
-    qUpdated[3] = vphiNew
-    #New r dot
-    vrNew = vr(rNew,phiNew,vphiNew,p,e)
-    qUpdated[2] = vrNew
-    return qUpdated
 
 #Define how to pass from polar to cartesian
 def polarToCartesian(r,theta):
